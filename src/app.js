b14d0e48ceebd35521cc948fed601a501b6444b6
DAVID <dpunzel15@georgefox.edu>
2019-02-08 20:23:34 -0800

    picking an option

--- src/app.js
+++ src/app.js
@@ -23,6 +23,11 @@ const onRemoveAll = () => {
     render();
 };
 
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    console.log(option);
+};
 const appRoot = document.getElementById('app');
 
 const numbers = [55, 101, 1000];
@@ -34,7 +39,9 @@ const render = () => {
             {app.subtitle && <p>{app.subtitle}</p>}
             {/*<p>{app.subtitle}</p>*/}
             <p>{app.options.length > 0 ? 'Here are your options' + ' ' + app.options: 'No options'}</p>
-            <p>{app.options.length}</p>
+            {/*No longer needed.*/}
+            {/*<p>{app.options.length}</p>*/}
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
             <button onClick={onRemoveAll}>Remove All</button>
             {/*{*/}
                 {/*numbers.map((number) => {*/}
