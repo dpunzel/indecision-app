fb950d2dfe77d43850ecdcd94cdf289dc9643d14
DAVID <dpunzel15@georgefox.edu>
2019-02-11 14:05:06 -0800

    React Components start

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -8,16 +8,50 @@ class Person {
     getGreeting() {
         // return 'Hi I am ' + this.name + '!';
         // es6 template strings
-        return `Hi. I am ${ this.name }!`;
+        return `Hi. I am ${ this.name }.`;
     }
     getDescription() {
         return `${this.name} is ${this.age} years(s) old. `;
     }
 }
 
-const me = new Person('David', 44);
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let desciption = super.getDescription();
+        if (this.hasMajor()) {
+            desciption += `${this.major}.`
+        }
+        return super.getDescription();
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLaction = homeLocation;
+    }
+    getGreeting() {
+        let greeting = super.getDescription();
+        if (this.homeLaction) {
+            greeting += ` I am visting from ${this.homeLaction}.`;
+        }
+        return greeting;
+    }
+}
+
+const me = new Student('David', 44, 'Computer Science');
+console.log(me);
 console.log(me.getGreeting());
 console.log(me.getDescription());
-const other = new Person();
+console.log(me.hasMajor());
+const other = new Student();
+console.log(other);
 console.log(other.getGreeting());
 console.log(other.getDescription());
