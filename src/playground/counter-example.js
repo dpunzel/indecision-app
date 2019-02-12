3fd7df8074b03b32a030c9d499e89c25341ba19b
DAVID <dpunzel15@georgefox.edu>
2019-02-11 23:26:37 -0800

    State counter

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,47 +1,95 @@
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterApp();
-    console.log('addOne', count);
-};
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-    console.log('minusOne');
-};
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-    console.log('reset')
-};
-// const templateTwo = (
-//     <div>
-//         <h1>Count: {count}</h1>
-//         <button onClick={addOne}>+1</button>
-//         <button onClick={minusOne}>-1</button>
-//         <button onClick={reset}>reset</button>
-//     </div>
-// );
-// console.log(templateTwo);
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count: 0
+        };
+    }
+    handleAddOne() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count + 1
+            };
+        });
+    }
 
-// Challenge
-// Make button "-1" setup minusOne function and register - log "minusOne"
-// Make reset button "reset" - setup reset function
-const appRoot = document.getElementById('app');
-// ReactDOM.render(template, appRoot);
-// ReactDOM.render(templateTwo, appRoot);
-// ReactDOM.render(templateTwo, appRoot);
+    handleMinusOne() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            };
+        });
+    }
 
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    );
-    ReactDOM.render(templateTwo, appRoot);
-};
+    handleReset() {
+        this.setState(() => {
+            return {
+                count: 0
+            };
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Count:{this.state.count}</h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>reset</button>
+            </div>
+        );
+    }
+}
 
-renderCounterApp();
+ReactDOM.render(<Counter />, document.getElementById('app'));
+
+
+// let count = 0;
+// const addOne = () => {
+//     count++;
+//     renderCounterApp();
+//     console.log('addOne', count);
+// };
+// const minusOne = () => {
+//     count--;
+//     renderCounterApp();
+//     console.log('minusOne');
+// };
+// const reset = () => {
+//     count = 0;
+//     renderCounterApp();
+//     console.log('reset')
+// };
+// // const templateTwo = (
+// //     <div>
+// //         <h1>Count: {count}</h1>
+// //         <button onClick={addOne}>+1</button>
+// //         <button onClick={minusOne}>-1</button>
+// //         <button onClick={reset}>reset</button>
+// //     </div>
+// // );
+// // console.log(templateTwo);
+//
+// // Challenge
+// // Make button "-1" setup minusOne function and register - log "minusOne"
+// // Make reset button "reset" - setup reset function
+// const appRoot = document.getElementById('app');
+// // ReactDOM.render(template, appRoot);
+// // ReactDOM.render(templateTwo, appRoot);
+// // ReactDOM.render(templateTwo, appRoot);
+//
+// const renderCounterApp = () => {
+//     const templateTwo = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={minusOne}>-1</button>
+//             <button onClick={reset}>reset</button>
+//         </div>
+//     );
+//     ReactDOM.render(templateTwo, appRoot);
+// };
+//
+// renderCounterApp();
