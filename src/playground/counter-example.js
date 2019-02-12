592cf317f6805aa440df93b4238472b3a8f13ea7
DAVID <dpunzel15@georgefox.edu>
2019-02-11 23:37:46 -0800

    state

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -30,6 +30,18 @@ class Counter extends React.Component {
                 count: 0
             };
         });
+        // this.setState((prevState) => {
+        //     return {
+        //         count: prevState.count + 1
+        //     };
+        // });
+        // old way with issues
+        // this.setState({
+        //     count: 0
+        // });
+        // this.setState({
+        //     count: this.state.count + 1
+        // });
     }
     render() {
         return (
